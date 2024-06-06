8c93788c3042dd6b401c0a93d17e34e1d4414e9a
Delta <46466697+DeltaGW2@users.noreply.github.com>
2024-06-06 19:06:38 +0200

    Update Mumble.h

--- Mumble.h
+++ Mumble.h
@@ -57,15 +57,16 @@ namespace Mumble
 
 	enum class EProfession : unsigned char
 	{
-		Guardian		= 1,
-		Warrior			= 2,
-		Engineer		= 3,
-		Ranger			= 4,
-		Thief			= 5,
-		Elementalist	= 6,
-		Mesmer			= 7,
-		Necromancer		= 8,
-		Revenant		= 9
+		None,
+		Guardian,
+		Warrior,
+		Engineer,
+		Ranger,
+		Thief,
+		Elementalist,
+		Mesmer,
+		Necromancer,
+		Revenant
 	};
 
 	enum class ERace : unsigned char
